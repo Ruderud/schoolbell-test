b6837523633a9ca9046c4e47f547d5514835ddcb
정경훈 <ruderud0055@gmail.com>
2023-12-19 12:02:55 +0900

    fix: tailwindcss config 파일 누락분 추가

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./Q3/index.html', './Q3/src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
